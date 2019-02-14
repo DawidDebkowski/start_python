98f9ee8f2abc18c2a28d73506d7591cb3ade6fd4
Dawid <dawid.debo2.pl@gmail.com>
2019-02-14 19:49:51 +0100

    Making caalc cli

--- witam_pana.py
+++ witam_pana.py
@@ -3,8 +3,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Powiedz: Witam serdecznie pana --name')
 parser.add_argument('--name', type=str, help='Pańskie imię proszę Pana!')
+parser.add_argument("--secondname", type=str, help='Pańskie nazwisko Panie! --secondname')
 
 
 args = parser.parse_args()
 
-print("Witam serdzecznie Pana " + args.name)
+print("Witam serdzecznie Pana o imieniu " + args.name)
+print("Witam serdzecznie Pana o nazwisku" + args.secondname)
+print("Witam serdzecznie Pana " + args.name + args.secondname)
